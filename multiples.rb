3132cf2ed0eca14338951b442f947e4e46664c3e
adyrda2 <adyrda2@gmail.com>
2016-01-08 19:02:19 -0600

    method working

---
+++ multiples.rb
@@ -0,0 +1,10 @@
+def sum_of_multiples(number)
+  numbers_array = (1..(number -1))
+  multiples_array = []
+  numbers_array.each do |num|
+    if num % 3 == 0 || num % 5 == 0
+       multiples_array << num
+      end
+    end
+  multiples_array.inject{|sum,x| sum + x }
+end
