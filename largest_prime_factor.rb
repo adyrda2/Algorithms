6b3223e93c655c5b13d44a215604e7640a1e85d4
adyrda2 <adyrda2@gmail.com>
2016-02-13 17:46:43 -0600

    largest prime factor solution

---
+++ largest_prime_factor.rb
@@ -0,0 +1,12 @@
+require 'Prime'
+def largest_prime(number)
+	primes_array = []
+	Prime.each(number) do |prime_num|
+	  if number % prime_num == 0 
+	  	primes_array << prime_num
+	    end
+	  end 
+p primes_array.last
+end 
+
+largest_prime(13195)
