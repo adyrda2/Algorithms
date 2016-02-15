1b1cd9b7c070e7eda0c563512c955b73761c3f68
Angela Dyrda <adyrda2@gmail.com>
2016-02-15 10:16:05 -0600

    Update largest_prime_factor.rb

--- largest_prime_factor.rb
+++ largest_prime_factor.rb
@@ -1,11 +1,11 @@
 require 'Prime'
 def largest_prime(number)
-	primes_array = []
-	Prime.each(number) do |prime_num|
-	  if number % prime_num == 0 
-	  	primes_array << prime_num
-	    end
-	  end 
+  primes_array = []
+  Prime.each(number) do |prime_num|
+  if number % prime_num == 0 
+    primes_array << prime_num
+    end
+  end 
 p primes_array.last
 end 
 
