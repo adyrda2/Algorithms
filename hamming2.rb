5e7a02879d39330e97eb7e91a1fb2654ab59b13d
adyrda2 <adyrda2@gmail.com>
2016-04-09 23:37:04 -0500

    Algorithm solutions

---
+++ hamming2.rb
@@ -0,0 +1,13 @@
+class Hamming
+  VERSION = 1
+  def self.compute(string_one, string_two)
+    raise ArgumentError if string_one.length != string_two.length
+    index = 0
+    counter = 0
+  while (first = string_one[index]) && (second = string_two[index])
+    counter += 1 if first != second
+    index +=1
+  end
+  return counter
+  end
+end
