2df7f2cf6046926ae317413584ed5f2c8d9bf481
Angela Dyrda <adyrda2@gmail.com>
2016-04-10 12:23:39 -0500

    Update sum_of_divisors.rb

--- sum_of_divisors.rb
+++ sum_of_divisors.rb
@@ -1,16 +1,16 @@
 def oddity(n)
-	possible_divisors = (1..n)
-	divisors = []
-	possible_divisors.each do |num|
-	  divisors << num unless n % num != 0 
-		end
-	divisors.length % 2 == 0 ? "even" : "odd"
+  possible_divisors = (1..n)
+  divisors = []
+  possible_divisors.each do |num|
+    divisors << num unless n % num != 0 
+  end
+  divisors.length % 2 == 0 ? "even" : "odd"
 end
 
 
 #Refactored solution 
 
 def oddity(n)
-	possible_divisors = (1..n).select{ |num| n % num == 0 } 
-	possible_divisors.length % 2 == 0 ? "even" : "odd"
+  possible_divisors = (1..n).select{ |num| n % num == 0 } 
+  possible_divisors.length % 2 == 0 ? "even" : "odd"
 end
